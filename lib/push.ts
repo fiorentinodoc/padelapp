caa8f4341127db9a7b86178b11dbe2d5aed02d08
Giorgio Gori <gio.gori@gmail.com>
2026-05-14 17:16:57 +0200

    fix push.ts ArrayBuffer type

--- lib/push.ts
+++ lib/push.ts
@@ -48,9 +48,14 @@ export async function registerPush(supabase: any, profileId: string): Promise<bo
   }
 }
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): ArrayBuffer {
   const padding = '='.repeat((4 - base64String.length % 4) % 4)
   const base64  = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
   const rawData = window.atob(base64)
-  return new Uint8Array([...rawData].map(c => c.charCodeAt(0)))
+  const buffer  = new ArrayBuffer(rawData.length)
+  const view    = new Uint8Array(buffer)
+  for (let i = 0; i < rawData.length; ++i) {
+    view[i] = rawData.charCodeAt(i)
+  }
+  return buffer
 }
